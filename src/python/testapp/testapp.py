2f86ef103987c3ae3985ea08ef2a9a4dec31f981
Katie Lucas <katie.lucas@fetch.ai>
2019-01-23 12:09:53 +0000

    Add bottle. Add SSL Http. Add to testapp.

--- src/python/testapp/testapp.py
+++ src/python/testapp/testapp.py
@@ -1,15 +1,81 @@
 #!/usr/bin/env python
 
-from src.python.testapp.mainfuncs import say_hello
+import functools
+import os
+import threading
+import sys
+
 from src.protos.testapp import a_pb2
+from src.python.testapp.mainfuncs import say_hello
+from src.third_party.bottle import SSLWSGIRefServer
+from src.third_party.bottle.bottle import abort
+from src.third_party.bottle.bottle import Bottle
+from src.third_party.bottle.bottle import error
+from src.third_party.bottle.bottle import hook
+from src.third_party.bottle.bottle import redirect
+from src.third_party.bottle.bottle import request
+from src.third_party.bottle.bottle import response
+from src.third_party.bottle.bottle import route
+from src.third_party.bottle.bottle import run
+from src.third_party.bottle.bottle import static_file
+from src.third_party.bottle.bottle import template
+
+# Build me like this:
+#   bazel build src/python/testapp:testapp
+#
+# Run me from the Workspace root like this:
+#   ./bazel-out/darwin-fastbuild/bin/src/python/testapp/testapp 5000 src/resources/testapp/ssl/server.pem 
+#
+
+certificate_file = None
+port_number = None
+
+def getStatic(filepath):
+    root = [
+        x for x in [
+            './static/',
+        ]
+        if os.path.exists(os.path.join(str(x), str(filepath)))
+    ]
+    if not root:
+        abort(404, "No such file.")
+    return static_file(filepath, root=root[0])
+
+def getRoot():
+    print("")
+    print(" GET ROOT")
+    print("")
+    print("")
+    msg = say_hello.say_hello()
+    print("MSG={}".format(repr(msg)))
+    r = a_pb2.A()
+    r.ParseFromString(msg)
+    return r.data
+
+def startWebServer(app):
+    srv = SSLWSGIRefServer.SSLWSGIRefServer(host="0.0.0.0", port=port_number, certificate_file=certificate_file)
+    run(server=srv, app=app)
 
 def main():
+
+    global port_number
+    global certificate_file
+
+    port_number = sys.argv[1]
+    certificate_file = sys.argv[2]
+
     msg = say_hello.say_hello()
     print("MSG={}".format(repr(msg)))
     result = a_pb2.A()
     result.ParseFromString(msg)
     print(result.data)
 
+    app = Bottle()
+
+    app.route('/', method='GET', callback=functools.partial(getRoot))
+    app.route('/static/<filepath:path>', method='GET', callback=functools.partial(getStatic))
+
+    startWebServer(app)
+
 if __name__ == "__main__":
     main()
-
---
+++ src/third_party/bottle/SSLWSGIRefServer.py
@@ -0,0 +1,22 @@
+import bottle
+
+class SSLWSGIRefServer(bottle.ServerAdapter):
+    def __init__(self, certificate_file=None, **kwargs):
+        self.certificate_file = certificate_file
+        super(SSLWSGIRefServer, self).__init__(**kwargs)
+
+    def run(self, handler):
+        from wsgiref.simple_server import make_server, WSGIRequestHandler
+        import ssl
+        import bottle
+        if self.quiet:
+            class QuietHandler(WSGIRequestHandler):
+                def log_request(*args, **kw):
+                    print(args, kw)
+            self.options['handler_class'] = QuietHandler
+        srv = make_server(self.host, self.port, handler, **self.options)
+        srv.socket = ssl.wrap_socket (
+            srv.socket,
+            certfile=self.certificate_file,  # path to certificate
+            server_side=True)
+        srv.serve_forever()
