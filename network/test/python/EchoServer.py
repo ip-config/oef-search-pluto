9090d2b2e5b5c7ea63c374328b9064babda9ad39
Attila Bagoly <b.attila.93@gmail.com>
2019-01-24 13:25:42 +0000

    PB to socket, not working

--- network/test/python/EchoServer.py
+++ network/test/python/EchoServer.py
@@ -1,5 +1,5 @@
 from network.src.python.async_socket.AsyncSocket import run_server, communication_handler, Writer
-
+import api.proto.py_pb_query
 
 @communication_handler
 def handler(data: bytes, writer: Writer):
