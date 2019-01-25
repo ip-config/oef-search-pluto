c242f39f65f0933e4e43f5afa7d5e4d78f59a77d
Katie Lucas <katie.lucas@fetch.ai>
2019-01-25 14:57:12 +0000

    Started laying out code for the Data Access Provider API.

---
+++ dap_api/src/python/DapInterface.py
@@ -0,0 +1,43 @@
+
+# We don't really need this, because it's DUCK TYPED, but this is the
+# interface definition for reference.
+
+class DapInterface(object)
+    def __init__():
+        pass
+
+    """This function returns the DAP description which lists the
+    tables it hosts, the fields within those tables and the result of
+    a lookup on any of those tables.
+
+    Returns:
+       DapDescription
+    """
+    def describe(self):
+        pass
+
+
+    """This function queries one or more tables in this DAP, applying filtering and returns
+    a list of all matching Agents which are in the pre-filtered list.
+
+    Args:
+      query (DapQuery): A query subtree which can be handled by this DAP.
+      agents (DapResult): A sub-result which can be used to optimise or post-filter the results or NONE.
+
+    Returns:
+      DapResult
+    """
+    def query(self, query, agents=None):
+        pass
+
+
+    """This function will be called with any update to this DAP.
+
+    Args:
+      update (DapUpdate): The update for this DAP.
+
+    Returns:
+      None
+    """
+    def query(self, query, agents=None):
+        pass
