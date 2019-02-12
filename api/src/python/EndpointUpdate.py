77a7432828b51c66d39bf246b56566f8d96b0303
Attila Bagoly <b.attila.93@gmail.com>
2019-02-12 12:20:36 +0000

    Improve update response

--- api/src/python/EndpointUpdate.py
+++ api/src/python/EndpointUpdate.py
@@ -4,7 +4,33 @@ from utils.src.python.Logging import has_logger
 from api.src.proto import update_pb2
 from api.src.proto import response_pb2
 from dap_api.src.python.DapManager import DapManager
-from api.src.python.ProtoWrappers import ProtoWrapper, InvalidAttribute
+from api.src.python.ProtoWrappers import ProtoWrapper, InvalidAttribute, MissingAddress
+
+ResponseType = response_pb2.UpdateResponse.ResponseType
+
+
+def process_update(self, msg):
+    resp = response_pb2.UpdateResponse()
+    try:
+        upd = self.proto_wrapper.get_instance(msg)
+        self.dap_manager.update(upd.toDapUpdate())
+        resp.status = ResponseType.Value("SUCCESS")
+    except InvalidAttribute as e:
+        msg = "Got invalid attribute: " + str(e)
+        resp.status = ResponseType.Value("INVALID_ATTRIBUTE")
+        resp.message = msg
+        self.log.info(msg)
+    except MissingAddress as e:
+        resp.status = ResponseType.Value("MISSING_ADDRESS")
+        msg = str(e)
+        resp.message = msg
+        self.log.info(msg)
+    except Exception as e:
+        msg = "Failed to update data, because: " + str(e)
+        resp.status = ResponseType.Value("ERROR")
+        resp.message = msg
+        self.log.info(msg)
+    return resp
 
 
 class UpdateEndpoint(HasProtoSerializer, HasMessageHandler):
@@ -22,18 +48,7 @@ class UpdateEndpoint(HasProtoSerializer, HasMessageHandler):
         pass
 
     async def handle_message(self, msg: update_pb2.Update) -> response_pb2.UpdateResponse:
-        resp = response_pb2.UpdateResponse()
-        try:
-            upd = self.proto_wrapper.get_instance(msg)
-            self.dap_manager.update(upd.toDapUpdate())
-            resp.status = 0
-        except InvalidAttribute as e:
-            self.log.warn("Got invalid attribute: " + str(e))
-            resp.status = 1
-        except Exception as e:
-            self.log.info("Failed to update data, because: ", e)
-            resp.status = 1
-        return resp
+        return process_update(self, msg)
 
 
 class BlkUpdateEndpoint(HasProtoSerializer, HasMessageHandler):
@@ -51,15 +66,4 @@ class BlkUpdateEndpoint(HasProtoSerializer, HasMessageHandler):
         pass
 
     async def handle_message(self, msg: update_pb2.Update.BulkUpdate) -> response_pb2.UpdateResponse:
-        resp = response_pb2.UpdateResponse()
-        try:
-            upd = self.proto_wrapper.get_instance(msg)
-            self.dap_manager.update(upd.toDapUpdate())
-            resp.status = 0
-        except InvalidAttribute as e:
-            self.log.warn("Got invalid attribute: "+str(e))
-            resp.status = 1
-        except Exception as e:
-            self.log.info("Failed to update data, because: "+str(e))
-            resp.status = 1
-        return resp
+        return process_update(self, msg)
