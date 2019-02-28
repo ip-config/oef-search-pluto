021fc5c32d694d3bdef2490fb4dbd45528fa5d69
Attila Bagoly <b.attila.93@gmail.com>
2019-02-28 13:50:31 +0000

    SearchNetwork and central pluto

--- pluto_app/src/python/main/central.py
+++ pluto_app/src/python/main/central.py
@@ -1,48 +1,43 @@
 #!/usr/bin/env python3
 from utils.src.python.Logging import configure as configure_logging
-from pluto_app.src.python.app import PlutoApp
 import argparse
-from typing import List
-import asyncio
+from pluto_app.src.python.app import SearchNetwork
 from api.src.python.CommunicationHandler import socket_server, http_server, CommunicationHandler
-from api.src.python.Interfaces import HasMessageHandler
+from fake_oef.src.python.lib import ConnectionFactory
+from fake_oef.src.python.lib import FakeAgent
+from api.src.proto import update_pb2
+from fetch_teams.oef_core_protocol import query_pb2
 
 
-class BroadcastFromNode(HasMessageHandler):
-    def __init__(self, path, network, node_id):
-        self._node = node_id
-        self._network = network
-        self._path = path
+def get_attr_b(name, desc, t=2):
+    attr1 = query_pb2.Query.Attribute()
+    attr1.name = name
+    attr1.type = t
+    attr1.required = True
+    attr1.description = desc
+    return attr1
 
-    async def handle_message(self, msg):
-        return await self._network.broadcast_from_node(self._node, self._path, msg)
+
+def create_update(key: str, name: str, description: str, attributes: list) -> update_pb2.Update:
+    upd = update_pb2.Update()
+    upd.key = key.encode("utf-8")
+    dm = query_pb2.Query.DataModel()
+    dm.name = name
+    dm.description = description
+    dm.attributes.extend(attributes)
+    upd.data_models.extend([dm])
+    return upd
 
 
-class SearchNetwork:
-    def __init__(self, coms={}, number_of_nodes=10):
-        self.nodes = []
-        for i in range(number_of_nodes):
-            app = PlutoApp.PlutoApp()
-            self.nodes.append(app)
-            app.run(coms.get(i, None))
-            app.add_handler("search", BroadcastFromNode("search", self, i))
-        self.connection_map = {}
-        self.working = {}
-
-    def set_connection(self, node: int, connections: List[int]):
-        self.connection_map[node] = connections
-
-    async def broadcast_from_node(self, node: int, path: str, data):
-        self.working = {node: 1}
-        cos = []
-        for i in self.connection_map[node]:
-            self.working[i] = self.working.get(i, 0) + 1
-            cos.append(self.nodes[i].callMe(path, data))
-        return await asyncio.gather(*cos)
-
-    def call_node(self, node: int, path: str, data):
-        self.working = {node: 1}
-        asyncio.run(self.nodes[node].callMe(path, data))
+def create_weather_dm_update(oef) -> update_pb2.Update.BulkUpdate:
+    upd1 = create_update(oef,
+                         "weather_data",
+                         "All possible weather data.", [
+                             get_attr_b("wind_speed", "Provides wind speed measurements.", 0),
+                             get_attr_b("temperature", "Provides wind speed measurements.", 1),
+                             get_attr_b("air_pressure", "Provides wind speed measurements.", 2)
+                         ])
+    return upd1
 
 
 if __name__ == "__main__":
@@ -58,14 +53,26 @@ if __name__ == "__main__":
     com = CommunicationHandler(1)
     com.add(http_server, "0.0.0.0", args.http_port, args.ssl_certificate, args.html_dir)
 
-    search_network = SearchNetwork({0: com}, 5)
+    search_network = SearchNetwork.SearchNetwork({0: com}, 5)
 
     # Search connectivity
-    search_network.set_connection(0, [1, 4])
-    search_network.set_connection(1, [2, 3])
-    search_network.set_connection(2, [3, 4])
-    search_network.set_connection(3, [4, 0])
-    search_network.set_connection(4, [1, 2])
+    #search_network.set_connection(0, [1, 4])
+    #search_network.set_connection(1, [2, 3])
+    #search_network.set_connection(2, [3, 4])
+    #search_network.set_connection(3, [4, 0])
+    #search_network.set_connection(4, [1, 2])
+
+    connection_factory = ConnectionFactory.ConnectionFactory()
+
+    oef1 = search_network.create_oef_core_for_node(0, "oef1", connection_factory)
+    oef1.register_oef_address(1)
+
+    agent = FakeAgent.FakeAgent(connection_factory=connection_factory, id='a0')
+    agent.connect(target="oef1", subject=None)
+
+    agent.register_service(create_weather_dm_update("oef1"))
+
+    com.wait()
 
 
 
