b473671e6c65ad5f4fae82a470ce9f96cdc8e045
Katie Lucas <katie.lucas@fetch.ai>
2019-03-01 13:31:05 +0000

    adding ability to run fake objects

--- fake_oef/src/python/lib/FakeBase.py
+++ fake_oef/src/python/lib/FakeBase.py
@@ -1,3 +1,23 @@
 class FakeBase(object):
+
+    objects = set()
+
     def __init__(self, *args, **kwargs):
-        pass
+        FakeBase.objects.add(self)
+
+    def visit(function,
+                fakebase_type_filter = None,
+                *args, **kwargs):
+        return [
+            function(x, *args, **kwargs)
+            for x in FakeBase.objects
+        ]
+
+    def filter(function,
+                fakebase_type_filter = None,
+                *args, **kwargs):
+        return [
+            x
+            for x in FakeBase.objects
+            if function(x, *args, **kwargs)
+        ]
