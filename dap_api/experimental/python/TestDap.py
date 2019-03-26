0a07568295e5ce4991bff54e5d8922d71e9bc327
Attila Bagoly <b.attila.93@gmail.com>
2019-03-26 15:13:42 +0000

    tesdap and small fix

--- dap_api/experimental/python/TestDap.py
+++ dap_api/experimental/python/TestDap.py
@@ -27,6 +27,7 @@ def create_dm(name: str, description: str, attributes: list) -> query_pb2.Query.
     dm.attributes.extend(attributes)
     return dm
 
+
 def create_dm_updates(upd):
     dm = create_dm("weatherdata",
                    "All possible weather data.", [
@@ -42,10 +43,37 @@ def create_dm_updates(upd):
     upd.key.agent = "Agent001".encode("utf-8")
 
 
+def add_value(upd, name, val):
+    v1 = upd.value.kv.add()
+    v1.key = name
+    if type(val) == int:
+        v1.value.i = val
+    elif type(val) == float:
+        v1.value.d = val
+    elif type(val) == str:
+        v1.value.s = val
+    elif type(val) == bool:
+        v1.value.b = val
+    elif type(val) == dict:
+        v1.value.l.lon = val["lon"]
+        v1.value.l.lat = val["lat"]
+
+
+def create_kv_updates(upd):
+    upd.tablename = "tablename"
+    upd.fieldname = "fieldname"
+    upd.value.type = 11
+    upd.key.core = "LondonCore".encode("utf-8")
+    upd.key.agent = "Agent001".encode("utf-8")
+    add_value(upd, "windspeed", 100.)
+    add_value(upd, "temperature", 100.)
+    #add_value(upd, "airpressure", 1.)
+
+
 def create_dap_updates() -> List[dap_update_pb2.DapUpdate]:
     update = dap_update_pb2.DapUpdate()
-    upd = update.update.add()
-    create_dm_updates(upd)
+    create_dm_updates(update.update.add())
+    create_kv_updates(update.update.add())
     return [update]
 
 
@@ -72,3 +100,5 @@ for name, conf in config_contract.items():
             print(upd)
             ss = upd.SerializeToString()
             print(dap.update(upd))
+        print("------------------\nREMOVE AKA QUERY")
+        print(dap.remove(updates[0]))
