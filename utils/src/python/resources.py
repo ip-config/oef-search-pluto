d1c7689e3f4da9847b6f20bc34bd53e7aec100d4
Katie Lucas <katie.lucas@fetch.ai>
2019-04-11 11:37:52 +0100

    More isolating work

--- utils/src/python/resources.py
+++ utils/src/python/resources.py
@@ -71,19 +71,20 @@ def resource(resourceName, as_string=False, as_file=True, open_mode="rb"):
             os.path.join(configured_filebase, "external", resourceName),
             os.path.join("external", resourceName),
         ]:
-            #print("Exists?", filename)
+            print("Exists?", filename)
             if os.path.exists(filename):
-                #print("Yes")
+                print("Yes")
                 detected_mode = 'filesystem'
                 if as_file:
-                    #print("returning file")
+                    print("returning file")
                     return open(filename, open_mode)
                 if as_string:
-                    #print("returning name")
+                    print("returning name")
                     return filename
                 with open(filename, open_mode) as binary_file:
                     return binary_file.read()
-        raise Exception("no")
+        if detected_mode in [ 'filesystem', 'bazel' ]:
+            raise Exception("no")
 
     detected_mode = 'loader'
     #print("R",package, resourceName)
