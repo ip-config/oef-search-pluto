670441136f0767a7b45c38e591fe06cbc43fe976
Katie Lucas <katie.lucas@fetch.ai>
2019-04-24 14:55:31 +0100

    switch to os-grid distancing

--- pluto_app/src/python/app/PlutoApp.py
+++ pluto_app/src/python/app/PlutoApp.py
@@ -62,6 +62,7 @@ class PlutoApp:
                                     "type": "location",
                                     "options": [
                                         "plane",
+                                        "os-grid",
                                     ]
                                 }
                             },
