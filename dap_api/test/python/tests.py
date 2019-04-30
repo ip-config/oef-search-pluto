619c17975145437cb310fd4e021edde9ade68062
Katie Lucas <katie.lucas@fetch.ai>
2019-04-30 16:35:58 +0100

    lazy daps test out

--- dap_api/test/python/tests.py
+++ dap_api/test/python/tests.py
@@ -4,11 +4,11 @@ import unittest
 #from dap_api.test.python.QueryEmbeddingsTest import QueryEmbeddingsTest
 
 
-#from dap_api.test.python.UpdateTest import UpdateTest
-#from dap_api.test.python.QueryTest import QueryTest
-#from dap_api.test.python.DapManagerTest import DapManagerTest
-#from dap_api.test.python.DapManagerMoreTest import DapManagerMoreTest
-#from dap_api.test.python.QueryDapFiltersTest import QueryDapFiltersTest
+from dap_api.test.python.UpdateTest import UpdateTest
+from dap_api.test.python.QueryTest import QueryTest
+from dap_api.test.python.DapManagerTest import DapManagerTest
+from dap_api.test.python.DapManagerMoreTest import DapManagerMoreTest
+from dap_api.test.python.QueryDapFiltersTest import QueryDapFiltersTest
 
 from dap_api.test.python.LazyDapTest import LazyDapTest
 
